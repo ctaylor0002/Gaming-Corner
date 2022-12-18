fdd729f5c7aa1acfd073ecb5ab27eb678795ae5f
Charles Taylor <charles4312@comcast.net>
2022-12-18 12:23:18 -0500

    Added Pillow Dependencies For ImageField + Made Migrations for Profile Model To Database

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+
+# <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
+
+
+# class Car(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     make = models.CharField(max_length=30)
+#     model = models.CharField(max_length=100)
+#     year = models.IntegerField()
+
+class User_Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField()
+    profile_description = models.CharField(max_length=1000)
+    #collection = models.ForeignKey(Collection, on_delete=models.CASCADE) (This will be used when I create the Colleciton App)
+    #follewers = models.ForeignKey(Followers, on_delete=models.CASCADE) (This will be used when I create the Followers App)
+    
