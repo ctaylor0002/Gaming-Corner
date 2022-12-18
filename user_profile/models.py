d156f45cb4d7ed84ab41034d8dd14fb142ff61fe
Charles Taylor <charles4312@comcast.net>
2022-12-18 12:55:08 -0500

    Created User_Collection Model + Made Database Migrations

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from authentication.models import User
+from video_game.models import Video_Game
 
 # Create your models here.
 
@@ -12,10 +13,15 @@ from authentication.models import User
 #     model = models.CharField(max_length=100)
 #     year = models.IntegerField()
 
+class User_Collection(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_game = models.ForeignKey(Video_Game, on_delete=models.CASCADE)
+
 class User_Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField()
     profile_description = models.CharField(max_length=1000)
-    #collection = models.ForeignKey(Collection, on_delete=models.CASCADE) (This will be used when I create the Colleciton App)
+    collection = models.ForeignKey(User_Collection, on_delete=models.CASCADE, default=None)
     #follewers = models.ForeignKey(Followers, on_delete=models.CASCADE) (This will be used when I create the Followers App)
-    
+
+
