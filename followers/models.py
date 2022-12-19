67667c415a7fc11c8d59ca23471ae9db72672aae
Charles Taylor <charles4312@comcast.net>
2022-12-19 17:44:17 -0500

    Registered All Models To Admin Site + Created user_collection App To Seperate From user_profile + Fixed Circular Import Error With Followers App

--- followers/models.py
+++ followers/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from authentication.models import User
-from user_profile.models import UserProfile
 
 # Create your models here.
 
 
 class UserFollowers(models.Model):
     main_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    follower_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    follower_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
     
---
+++ user_collection/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from authentication.models import User
+from video_game.models import VideoGame
+
+# Create your models here.
+
+class UserCollection(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_game = models.ForeignKey(VideoGame, on_delete=models.CASCADE)
