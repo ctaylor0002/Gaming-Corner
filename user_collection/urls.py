aea8b3907b60fd5c36bc0b2402702e2194d12a22
Charles Taylor <charles4312@comcast.net>
2022-12-23 22:16:58 -0500

    Created Method For Updating (Completing Games) Or Deleting Games From A Collection

--- user_collection/urls.py
+++ user_collection/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from user_collection import views
 
 urlpatterns = [
-    path('<int:user_id>/', views.user_collection),
+    path('<int:user_id>', views.user_collection),
+    path('<int:pk>/update/', views.update_user_collection),
 ]
--- user_collection/views.py
+++ user_collection/views.py
@@ -9,8 +9,8 @@ from rest_framework.decorators import api_view, permission_classes
 
 # Create your views here.
         
-@api_view(['GET','POST'])
-@permission_classes([AllowAny])
+@api_view(['GET','POST', 'PATCH', 'DELETE'])
+@permission_classes([IsAuthenticated])
 def user_collection(request, user_id):
     if (request.method == 'GET'):
         collection = UserCollection.objects.filter(user=user_id)
@@ -23,3 +23,16 @@ def user_collection(request, user_id):
             serializer.save(user=request.user, video_game_id=request.data["video_game_id"])
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PATCH', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def update_user_collection(request, pk):
+    video_game = get_object_or_404(UserCollection, pk=pk)
+    if (request.method == 'PATCH'):
+        serializer = UserCollectionSerializer(video_game, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK) 
+    elif (request.method == 'DELETE'):
+        video_game.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
