c50a1e8f0a943a3015a91ff98d7147a697c1328f
Charles Taylor <charles4312@comcast.net>
2022-12-23 21:47:26 -0500

    Added Method To Add Games To Collection Of A Specific User

--- user_collection/urls.py
+++ user_collection/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from user_collection import views
 
 urlpatterns = [
-    path('<int:user_id>/', views.get_collection),
+    path('<int:user_id>/', views.user_collection),
 ]
--- user_collection/views.py
+++ user_collection/views.py
@@ -9,12 +9,17 @@ from rest_framework.decorators import api_view, permission_classes
 
 # Create your views here.
         
-@api_view(['GET'])
+@api_view(['GET','POST'])
 @permission_classes([AllowAny])
-def get_collection(request, user_id):
-    collection = get_object_or_404(UserCollection, user=user_id)
-    # profile = UserProfile.objects.filter(pk=pk)
-    print(collection)
-    serializer = UserCollectionSerializer(collection)
-    print(serializer.data)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+def user_collection(request, user_id):
+    if (request.method == 'GET'):
+        collection = UserCollection.objects.filter(user=user_id)
+        serializer = UserCollectionSerializer(collection, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif (request.method == 'POST'):
+        serializer = UserCollectionSerializer(data=request.data)
+        # print(request.data["video_game_id"])
+        if serializer.is_valid():
+            serializer.save(user=request.user, video_game_id=request.data["video_game_id"])
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
