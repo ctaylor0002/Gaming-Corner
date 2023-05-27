d6708e49ea4d8e0b1ae756f85d2d54327617ed26
ctaylor0002 <charles4312@comcast.net>
2023-05-26 20:23:12 -0400

    Updated User Token Data

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -16,6 +16,11 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
         token["username"] = user.username
         token["first_name"] = user.first_name
+        token["display_name"] = user.display_name
+        token["profile_picture"] = str(user.profile_picture)
+        token["profile_description"] = user.profile_description
+
+        print(token.__dict__)
 
         return token
 
@@ -32,7 +37,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
         # If added new columns through the User model, add them in the fields
         # list as seen below
         fields = ('username', 'password', 'email',
-                  'first_name', 'last_name', 'profile_picture', 'profile_description')
+                  'first_name', 'last_name', 'profile_picture', 'profile_description', 'display_name')
 
     def create(self, validated_data):
 
@@ -41,6 +46,9 @@ class RegistrationSerializer(serializers.ModelSerializer):
             email=validated_data['email'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
+            display_name=validated_data['display_name'],
+            profile_picture=validated_data['profile_picture'],
+            profile_description=validated_data['profile_description'],
 
             # If added new columns through the User model, add them in this
             # create method. Example below:
